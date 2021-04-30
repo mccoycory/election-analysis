d5c9a3475d0feb50e11c4617dd48da47c98117a2
mccoycory <82479618+mccoycory@users.noreply.github.com>
2021-04-29 21:58:42 -0500

    Module Push

---
+++ Python_practice.py
@@ -0,0 +1,13 @@
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[3] != 'Jefferson':
+    print(counties[2])
+
+
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
